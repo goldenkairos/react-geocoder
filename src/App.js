20a9ad895292c8d51cb2dd9fad334b61d9cf0d8c
Minh <minh.seikel@gmail.com>
2022-12-22 21:36:53 -0800

    adding search history

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import NewLocationForm from "./components/NewLocationForm";
 import LocationList from "./components/LocationList";
+import Location from "./components/Location";
 import axios from "axios";
-// import API from "./.env"
-// import key from "./.env"
+// import keys from "./ignore";
+import keys from "./env";
+
 import { useState, useEffect } from "react";
+import { DateTime } from "luxon";
 // const axios = require("axios");
 // require('dotenv').config();
 // const INITIAL_CITY = "Seattle";
@@ -14,59 +17,52 @@ import { useState, useEffect } from "react";
 //   lon: -122.330062,
 // };
 
-const INITIAL_COORDINATES ={
-  name:"Seattle",
+const INITIAL_COORDINATES = {
+  name: "Seattle",
   lat: 47.6038321,
   lon: -122.330062,
-}
-const key = "pk.d3239da17a96e36bb9f66cb0184f44ec";
-
-const API = "https://us1.locationiq.com/v1/search.php";
+};
 
 function App() {
   const [coordinates, setCoordinates] = useState(INITIAL_COORDINATES);
-  // const [city, setCity] = useState(INITIAL_CITY);
+
+  const [cityList, setCityList] = useState([]);
 
   const searchCity = (newCity) => {
     console.log("searchCity called");
-    console.log(newCity)
-    axios.get(API, { params: { key:key, q: newCity, format: "json" } })
-    .then((result) => {
-      const locAPICopy = () => {
-        return {
-          name:newCity,
-          lat: result.data[0].lat,
-          lon: result.data[0].lon,
+    console.log(newCity);
+    axios
+      .get(keys.API, { params: { key: keys.key, q: newCity, format: "json" } })
+      .then((result) => {
+        const locAPICopy = () => {
+          return {
+            name: newCity,
+            lat: result.data[0].lat,
+            lon: result.data[0].lon,
+          };
         };
-      };
-      setCoordinates(locAPICopy);
-      console.log(locAPICopy)
-      // console.log("NEW COORDINATES", coordinates);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+        setCoordinates(locAPICopy);
+
+        setCityList([
+          ...cityList,
+          { name: newCity, lat: result.data[0].lat, lon: result.data[0].lon },
+        ]);
+        console.log(cityList);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-
-  // useEffect(() => {
-  //   axios
-  //   .get(API, { params: { key: key, q: city, format: "json" } })
-  //   .then((result) => {
-  //     const locAPICopy = () => {
-  //       return {
-  //         lat: result.data[0].lat,
-  //         lon: result.data[0].lon,
-  //       };
-  //     };
-  //     setCoordinates(locAPICopy);
-  //     // console.log("NEW COORDINATES", coordinates);
-  //   })
-  //   .catch((error) => {
-  //     console.log(error);
-  //   });
-  // }, [city]);
-
+    //we can create a seperate component here as HistoryList
+  const historyList = cityList.map((item) => {
+    return (
+      <div key={DateTime.now()}>
+        <h4>{item.name}</h4>
+        <Location coordinates={item} />
+      </div>
+    );
+  });
 
   return (
     <div className="App">
@@ -74,10 +70,18 @@ function App() {
         <h1>Geocoder</h1>
       </header>
       <main>
-        <NewLocationForm searchCityCallbackfunc={searchCity} coordinates={coordinates} />
+        <NewLocationForm
+          searchCityCallbackfunc={searchCity}
+          coordinates={coordinates}
+        />
         <section className="result">
           <h3> Result for:</h3>
           <LocationList coordinates={coordinates} />
+        </section>
+        <section>
+          <h3> Search History</h3>
+          {historyList}
+          {/* <HistoryList cityList={cityList} /> */}
         </section>
       </main>
     </div>
---
+++ src/components/HistoryList.js
@@ -0,0 +1,24 @@
+//     //we can create a seperate component here as HistoryList
+// import React from 'react';
+// import Location from './Location';
+
+// const HistoryList=(props) => {
+//   const cityList = props.cityList;
+
+//   const searchHistory = cityList.map((item)=> {
+//     return (
+//       <div key={item.name}>
+//       <h4>name={item.name}</h4>
+//       <Location 
+//         lat={item.lat}
+//         lon={item.lon}
+//       />
+//       </div>
+//     );
+//   })
+
+
+// };
+
+export default HistoryList;
+
